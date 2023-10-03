6bd8f021c5b176128ff52802c5733df852ad71bb
name <TurdubEln008@gmail.com>
2023-10-03 20:22:26 +0600

    asdfghj

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Java 11 list of students: ");
-        System.out.println("Elnura");
+        System.out.println("Elnuraaaaa");
     }
 }
