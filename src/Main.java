501a9755f88b0dd83d6feedddf7e9e4948dd0f44
practical <er@mycommit.com>
2023-10-03 18:43:20 +0600

    11

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Java 11 list of students: ");
-        System.out.println("erkin ");
 
+
+        System.out.println("java11");
 
     }
 }
