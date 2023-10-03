e27af26cda43bafbff1ad522824c98253d99f36b
JandarJavaSenior <xp3335@email.com>
2023-10-03 18:07:57 +0600

    jnad

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Java 11 list of students: ");
-        System.out.println("Java 11 list of students: ");
+        System.out.println("Jandar\n\n\n\n");
 
     }
 }
